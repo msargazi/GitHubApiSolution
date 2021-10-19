2c399936937708edd3e75743acc8f487ffa2fd8f
mohammad sragazi <m.sargazi1370@yahoo.com>
2021-10-19 03:14:14 -0700

    add model

---
+++ GitHubApi.Core/Entities/Repository.cs
@@ -0,0 +1,21 @@
+﻿using GitHubApi.Core.Entities.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GitHubApi.Core.Entities
+{
+    public class Repository : Entity
+    {
+        public int RepositoryId { get; set; }
+        public string NodeId { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public bool Private { get; set; }
+        public string Description { get; set; }
+
+        #region NavigationProp
+        public virtual User Owner { get; set; }
+        [ForeignKey("Owner")]
+        public int UserId { get; set; }
+        #endregion
+    }
+}
---
+++ GitHubApi.Core/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using GitHubApi.Core.Entities.Base;
+
+namespace GitHubApi.Core.Entities
+{
+    public class User : Entity
+    {
+        public string Login { get; set; }
+        public int UserId { get; set; }
+        public string NodeId { get; set; }
+        public string AvatarUrl { get; set; }
+
+    }
+}
